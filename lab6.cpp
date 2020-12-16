22deb191fa1f53dcb5499ab1d0c2b675a1b3ad32
miloszwnuk <01157070@pw.edu.pl>
2020-12-16 20:45:15 +0000

    yes

--- lab6.cpp
+++ lab6.cpp
@@ -1,6 +1,65 @@
+#include <algorithm>
 #include <iostream>
+#include <list>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Human
+{
+public:
+    Human() : name{"Anonymous"}, age{0}, likes_dogs{true}, likes_cats{true} {}
+    Human(std::string name_, unsigned int age_, bool ld, bool lk)
+        : name{std::move(name_)}, age{age_}, likes_dogs{ld}, likes_cats{lk}
+    {}
+
+    const std::string& getName() const { return name; }
+    unsigned int       getAge() const { return age; }
+    bool               isMonster() const { return !likes_dogs && !likes_cats; }
+    void               birthday() { ++age; }
+
+private:
+    std::string  name;
+    unsigned int age;
+    bool         likes_dogs;
+    bool         likes_cats;
+};
+
+std::vector< char > foo(std::list< Human >& people)
+{
+    std::vector< char > likesAnimals;
+    std::for_each(people.rbegin(), people.rend(), [&likesAnimals](Human& human) {
+        human.birthday();
+        if (human.isMonster() == true) {
+            likesAnimals.push_back('n');
+        }
+        else {
+            likesAnimals.push_back('y');
+        }
+    });
+
+    return likesAnimals;
+}
 
 int main()
 {
-    puts("Hello, World!");
+    char  b;
+    Human h1("A", 5, true, true);
+    Human h2("B", 10, true, false);
+    Human h3("C", 15, false, false);
+    Human h4("D", 20, false, true);
+
+    list< Human > humans{h1, h2, h3, h4};
+    auto          whoLikesAnimals = foo(humans);
+
+    for (char isMonster : whoLikesAnimals) {
+        cout << isMonster << endl;
+    }
+    cout << endl;
+    for (auto human : humans) {
+        cout << human.getAge() << endl;
+    }
+
+    return 0;
 }
--- zal/foo.hpp
+++ zal/foo.hpp
@@ -1,16 +1,27 @@
 #pragma once
 
 #include "Human.hpp"
+using namespace std;
 
-#include <algorithm>
+#include <iostream>
 #include <list>
+#include <string>
 #include <vector>
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    std::vector< char > ret_v(people.size());
+    std::vector< char > likesAnimals;
+    std::for_each(people.rbegin(), people.rend(), [&likesAnimals](Human& human) {
+        human.birthday();
+        if (human.isMonster() == true) {
+            likesAnimals.push_back('n');
+        }
+        else {
+            likesAnimals.push_back('y');
+        }
+    });
 
-    // Twoja implementacja tutaj
-
-    return ret_v;
+    return likesAnimals;
 }
+
+
